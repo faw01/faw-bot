f883975334bbbbb44746b167e845744ce7824507
faw <fawwad30@gmail.com>
2023-07-06 01:30:49 +0800

    added oj command

--- bot_commands.py
+++ bot_commands.py
@@ -26,3 +26,14 @@ class BotCommands(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f"pong! {round(self.bot.latency * 1000)} ms")
+
+    @commands.command()
+    async def oj(self, ctx):
+        embed = discord.Embed(title="Orange juice price (USD/Lbs)", color=0x1ABC9C)
+
+        embed.add_field(name="1st of January, 2021", value="$121", inline=False)
+        embed.add_field(name="1st of January, 2022", value="$146", inline=False)
+        embed.add_field(name="1st of January, 2023", value="$203", inline=False)
+        embed.add_field(name="Now", value="$285.75", inline=False)
+
+        await ctx.send(embed=embed)
